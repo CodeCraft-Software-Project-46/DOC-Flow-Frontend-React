8064ca1174440767cd9acb292a175e239bb1f8f0
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-04-28 23:21:05 +0530

    create function to get all permissions from backend

--- src/service/PermissionService.ts
+++ src/service/PermissionService.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api/user/permissions/";
+const BASE_URL = "http://localhost:8000/api/user/permission/";
 
 export const permissionService = {
 
     // GET all permissions
     getAll: async () => {
-        const res = await axios.get(BASE_URL, {
+        const res = await axios.get(`${BASE_URL}getAll/`, {
             headers: {
                 "Content-Type": "application/json",
             },
         });
 
         return res.data;
-    },
+    }
 };
