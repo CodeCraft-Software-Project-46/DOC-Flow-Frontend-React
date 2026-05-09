30c9ef2bdfb4e72d1cd4bf6cb3b65e6f5d88ae56
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-05-10 01:37:38 +0530

    mapping dashboard components with widget_code

---
+++ src/WIDGET_COMPONENTS.ts
@@ -0,0 +1,18 @@
+// src/widgets/WIDGET_COMPONENTS.ts
+
+
+import BottleneckSteps from "./components/chart/BottleneckSteps.tsx";
+import WorkflowPerformance from "./components/chart/WorkflowPerformance.tsx";
+import TaskSummary from "./components/chart/TaskSummary.tsx";
+import DocumentOverview from "./components/chart/DocumentOverview.tsx";
+import SystemAlerts from "./components/chart/SystemAlerts.tsx";
+import UserSLAList from "./components/chart/Userperformance.tsx";
+
+export const WIDGET_COMPONENTS: Record<string, any> = {
+    W001: BottleneckSteps,
+    W002: WorkflowPerformance,
+    W003: UserSLAList,
+    W004: TaskSummary,
+    W005: DocumentOverview,
+    W006: SystemAlerts,
+};
